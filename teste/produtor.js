536bf2ca5fa635827dd5e300519b7fa63457ff3f
marcelo <marcelohdjusto@hotmail.com>
2023-09-16 20:56:33 -0300

    NEW
    teste para google tag manager

---
+++ teste/produtor.js
@@ -0,0 +1,45 @@
+
+let produtos = [
+    {
+        id: 0,
+        nome: "Torta de Limão",
+        preco: 7.00,
+        imagem: "verdadeiro/img.jpg"
+    },
+    {
+        id: 1,
+        nome: "Palha Italiana",
+        preco: 7.00,
+        imagem: "verdadeiro/img2.jpeg"
+    },
+    {
+        id: 2,
+        nome: "Menta",
+        preco: 7.00,
+        imagem: "verdadeiro/img3.jpeg"
+    },
+    {
+        id: 3,
+        nome: "Cappuccino",
+        preco: 7.00,
+        imagem: "verdadeiro/img4.jpeg"
+    },
+    {
+        id: 4,
+        nome: "Chokito",
+        preco: 7.00,
+        imagem: "verdadeiro/img5.jpeg"
+    },
+    {
+        id: 5,
+        nome: "Sensação",
+        preco: 7.00,
+        imagem: "verdadeiro/img6.jpeg"
+    },
+    {
+        id: 6,
+        nome: "Brigadeiro",
+        preco: 7.00,
+        imagem: ""
+    }
+];
